06c4ace2daa59e37d65a172d521cbf809408fa56
Loukou Paul-Erwin Koffi <paulerwink@gmail.com>
2025-02-08 18:01:52 -0500

    Update Map and Add Circle

--- bluewatch/src/components/MapComponent.tsx
+++ bluewatch/src/components/MapComponent.tsx
@@ -1,17 +1,26 @@
-import { MapContainer, TileLayer} from 'react-leaflet';
+import { LatLngTuple } from 'leaflet';
+import { MapContainer, TileLayer, Circle} from 'react-leaflet';
 
 
 const MapComponent = () => {
-  const position: [number, number] = [51.505, -0.09]; 
-  
+  const position: [number, number] = [22.5, -89.5]; 
+  const circlePosition: LatLngTuple = [24.5, -92.5];
+
   return (
-    <MapContainer center={position} zoom={13} style={{ width: '100%', height: '500px' }}>
+    <MapContainer center={position} zoom={5}  minZoom={2} style={{ width: '100%', height: '500px' }}>
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-     
+         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+         attribution='&copy; <a href="https://www.esri.com/">Esri</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+       />
+       <Circle
+    center={circlePosition}
+    color="red" 
+    fillColor="#f03" 
+    fillOpacity={0.5} 
+    radius={400000} 
+  />
     </MapContainer>
+    
   );
 };
 
