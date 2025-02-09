22814027bedd05ada17017a96ce71e93af286694
Loukou Paul-Erwin Koffi <paulerwink@gmail.com>
2025-02-09 04:04:15 -0500

    Update Home

--- src/index.php
+++ src/index.php
@@ -3,26 +3,29 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Blue Watch</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+   
+  <!-- ***** Preloader End ***** -->
+
 <section class="hero-section">
   <div class="container">
     <div class="content">
       <h1 class="gradient-text">
-        Understand User Flow.
-        <span class="block"> Increase Conversion. </span>
+      Surveillez les Écosystèmes Marins.
+        <span class="block"> Protégez la biodiversité.</span>
       </h1>
 
       <p class="description">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-        numquam ea!
+      Une plateforme interactive permettant de suivre l&apos;état des océans, d&apos;analyser les données 
+      environnementales et de contribuer à la préservation des milieux marins grâce à une carte en temps réel.
       </p>
 
       <div class="buttons">
-        <a class="btn primary" href="map.php">Get Started</a>
-        <a class="btn secondary" href="#">Learn More</a>
+        <a class="btn primary" href="map.php">Explorez Notre Carte</a>
+        <a class="btn secondary" href="#">En Savoir Plus</a>
       </div>
     </div>
   </div>
