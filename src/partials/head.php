ca41996a46ebd91181cfb9d7175946e90c1e97f2
Loukou Paul-Erwin Koffi <paulerwink@gmail.com>
2025-02-09 01:20:19 -0500

    Add Partial Files

---
+++ src/partials/head.php
@@ -0,0 +1,29 @@
+<head>
+    <!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+<!-- Favicon icon-->
+<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon/favicon.ico">
+
+<!-- Libs CSS -->
+
+
+<link href="assets/libs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/libs/dropzone/dist/dropzone.css"  rel="stylesheet">
+<link href="assets/libs/%40mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
+<link href="assets/libs/prismjs/themes/prism-okaidia.css" rel="stylesheet">
+
+
+
+
+
+
+
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="assets/css/theme.min.css">
+    <title>Blue Watch</title>
+  </head>
