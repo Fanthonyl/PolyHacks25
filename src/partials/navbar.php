7c8e31632899522522a1ca7365269fa429ee6eb3
Loukou Paul-Erwin Koffi <paulerwink@gmail.com>
2025-02-09 04:23:50 -0500

    Add Logo

--- src/partials/navbar.php
+++ src/partials/navbar.php
@@ -1,8 +1,9 @@
 <nav class="navbar-vertical navbar">
     <div class="nav-scroller">
         <!-- Brand logo -->
-        <a class="navbar-brand" href="index.php">
-            <img src="assets/images/brand/logo/logo.svg" alt="" />
+        <a class="navbar-brand" href="index.php" > 
+            <img style="width: 100px; /* Ajustez la largeur selon vos besoins */
+        height: auto;" src="assets/images/brand/logo/logo.svg" alt="" />
         </a>
         <!-- Navbar nav -->
         <ul class="navbar-nav flex-column" id="sideNavbar">
